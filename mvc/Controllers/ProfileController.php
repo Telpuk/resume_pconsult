ce1b6cc82f6a07928a362a2142e7df5891d33a49
telpuk <telpuk.sergey@gmail.com>
2014-10-07 17:04:11 +0300

    init

--- mvc/Controllers/ProfileController.php
+++ mvc/Controllers/ProfileController.php
@@ -1,187 +1,205 @@
 <?php
-class ProfileController extends IController{
-	private $_view;
-	private $_dbuser;
-	private $_format_photo = ['png'=>'png','jpg'=>'jpg','gif' =>'gif'];
-	private $_size_photo = 6291456;
+	class ProfileController extends IController{
+		private $_view;
+		private $_dbuser;
+		private $_format_photo = ['png'=>'png','jpg'=>'jpg','gif' =>'gif'];
+		private $_size_photo = 6291456;
 
-	public function __construct(){
-		parent::__construct();
-		$this->_view = new View();
-		$this->_dbuser = new User();
-	}
-
-	private function _getDownloadDirPhoto(){
-		return DIR_PROJECT.'/files/photo';
-	}
-
-	public function photoAction(){
-		$photo_name = $this->_dbuser->selectPhotoID($this->getSessionUserID('user'));
-
-		return $this->_view->render(array(
-			'view' => 'profile/photo',
-			'data'=> array_merge( array(
-					'src'=>"/files/photo/{$photo_name}",
-					'submit'=>($photo_name === 'no-photo.png') ? true: false),
-				$this->_uploadsAction())
-		));
-	}
-
-	public function deleteAction(){
-		$this->_dbuser->updatePhotoId('no-photo.png', $this->getSessionUserID('user'));
-		@unlink($this->_getDownloadDirPhoto()."/".$this->getParams()['photo']);
-		$this->headerLocation('profile/photo');
-
-	}
-
-	public function personalAction(){
-
-		if(isset($_POST['savePersonal'])){
-			$_checkForm = $this->_checkFormPersonal($_POST);
-			foreach($_checkForm as $input){
-				if(!$input['val']){
-					break;
-				}
-			}
-		}else{
-			$_checkForm = array();
+		public function __construct(){
+			parent::__construct();
+			$this->_view = new View();
+			$this->_dbuser = new User();
 		}
 
-		return $this->_view->render(array(
-			'view' => 'profile/personal',
-			'data'=>array('inputs'=>$_checkForm),
-			'js'=>$this->_jsPersonal(),
-		));
-	}
+		private function _getDownloadDirPhoto(){
+			return DIR_PROJECT.'/files/photo';
+		}
 
-	protected function _checkFormPersonal($post){
-		$surname = isset($post['surname'])?trim(strip_tags(mb_eregi_replace('[^A-Za-zА-Яа-яёЁ]','',
-			$post['surname']))):'';
-		$first_name = isset($post['first_name'])?trim(strip_tags(mb_eregi_replace('[^A-Za-zА-Яа-яёЁ]','', $post['first_name']))):'';
-		$patronymic = isset($post['patronymic'])?trim(strip_tags(mb_eregi_replace('[^A-Za-zА-Яа-яёЁ]','', $post['patronymic']))):'';
+		public function photoAction(){
+			$photo_name = $this->_dbuser->selectPhotoID($this->getSessionUserID('user'));
 
-		$day_birth = isset($post['day_birth'])?trim(strip_tags($post['day_birth'])):'';
-		$month_birth = isset($post['month_birth'])?trim(strip_tags($post['month_birth'])):'';
-		$year_birth = isset($post['year_birth'])?trim(strip_tags($post['year_birth'])):'';
+			return $this->_view->render(array(
+				'view' => 'profile/photo',
+				'data'=> array_merge( array(
+						'src'=>"/files/photo/{$photo_name}",
+						'submit'=>($photo_name === 'no-photo.png') ? true: false),
+					$this->_uploadsAction())
+			));
+		}
 
-		$sex =  isset($post['sex'])?trim(strip_tags($post['sex'])):'';
-		$city = isset($post['city'])?trim(strip_tags(mb_eregi_replace('[^A-Za-zА-Яа-я]','',$post['city']))):'';
+		public function deleteAction(){
+			$this->_dbuser->updatePhotoId('no-photo.png', $this->getSessionUserID('user'));
+			@unlink($this->_getDownloadDirPhoto()."/".$this->getParams()['photo']);
+			$this->headerLocation('profile/photo');
 
-		$move = isset($post['move'])?trim(strip_tags($post['move'])):'';
-		$trip = isset($post['trip'])?trim(strip_tags($post['trip'])):'';
+		}
 
-		$nationality = isset($post['nationality'])?trim(strip_tags($post['nationality'])): '';
-		$nationality_other = isset($post['nationality_other'])?trim(strip_tags($post['nationality_other'])):'';
+		public function personalAction(){
 
-		$work_permit = isset($post['work_permit'])?trim(strip_tags($post['work_permit'])):'';
-		$work_permit_other = isset($post['$work_permit_other'])?trim(strip_tags($post['work_permit_other'])):'';
+			if(isset($_POST['savePersonal'])){
+				$checkForm = $this->_checkFormPersonal($_POST);
+				foreach($checkForm as $input){
+					if(isset($input['val']) && !$input['val']){
+						return $this->_view->render(array(
+							'view' => 'profile/personal',
+							'data'=>array('inputs'=>$checkForm),
+							'js'=>$this->_jsPersonal(),
+						));
+					}
+				}
+				$this->_dbuser->updatePersonal($checkForm, $this->getSessionUserID('user'));
+				$this->headerLocation('index');
+			}else{
+				$checkForm = $this->_dbuser->selectPersonal($this->getSessionUserID('user'));
+				return $this->_view->render(array(
+					'view' => 'profile/personal',
+					'data'=>array('inputs'=>$checkForm),
+					'js'=>$this->_jsPersonal(),
+				));
+			}
 
-		$travel_time_work = isset($post['travel_time_work'])?trim(strip_tags($post['travel_time_work'])):'';
+
+		}
+
+		protected function _checkFormPersonal($post){
+			$surname = isset($post['surname'])?trim(strip_tags(mb_eregi_replace('[^A-Za-zА-Яа-яёЁ]','', $post['surname']))):'';
+			$first_name = isset($post['first_name'])?trim(strip_tags(mb_eregi_replace('[^A-Za-zА-Яа-яёЁ]','', $post['first_name']))):'';
+			$patronymic = isset($post['patronymic'])?trim(strip_tags(mb_eregi_replace('[^A-Za-zА-Яа-яёЁ]','', $post['patronymic']))):'';
+
+			$day_birth = isset($post['day_birth'])?trim(strip_tags($post['day_birth'])):'';
+			$month_birth = isset($post['month_birth'])?trim(strip_tags($post['month_birth'])):'';
+			$year_birth = isset($post['year_birth'])?trim(strip_tags($post['year_birth'])):'';
+
+			$sex =  isset($post['sex'])?trim(strip_tags($post['sex'])):'';
+			$city = isset($post['city'])?trim(strip_tags(mb_eregi_replace('[^A-Za-zА-Яа-я]','',$post['city']))):'';
+
+			$move = isset($post['move'])?trim(strip_tags($post['move'])):'';
+			$trip = isset($post['trip'])?trim(strip_tags($post['trip'])):'';
+
+			$nationality = isset($post['nationality'])?trim(strip_tags($post['nationality'])): '';
+
+			$work_permit = isset($post['work_permit'])?trim(strip_tags($post['work_permit'])):'';
+
+			$travel_time_work = isset($post['travel_time_work'])?trim(strip_tags($post['travel_time_work'])):'';
+
+			$surname_val = call_user_func(function($surname){
+				return preg_match('/^[a-zA-Zа-яА-ЯёЁ]+$/ui',$surname)&&!empty($surname) ? true:
+					false;
+			}, $surname);
+
+			$first_name_val = call_user_func(function($first_name){
+				return preg_match('/^[a-zA-Zа-яА-ЯёЁ]+$/ui',$first_name)&&!empty($first_name) ?
+					true: false;
+			}, $first_name);
+
+			$patronymic_val = call_user_func(function($patronymic){
+				return preg_match('/^[a-zA-Zа-яА-ЯёЁ]+$/ui',$patronymic)||empty($patronymic)
+					? true: false;
+			}, $patronymic);
+
+			$birth_val = call_user_func(function($date){
+				return (is_numeric($date['day_birth'])&&
+					is_numeric($date['month_birth'])&&
+					is_numeric($date['year_birth'])) ||
+				(empty($date['day_birth'])&& empty($date['month_birth'])&& empty($date['year_birth']))? true: false;
+			}, array('day_birth'=>$day_birth, 'month_birth'=>$month_birth, 'year_birth'=>$year_birth));
+
+			$city_val = call_user_func(function($city){
+				return !empty($city) ? true : false;
+			},$city);
 
 
-		$surname_val = call_user_func(function($surname){
-			return preg_match('/^(?:[a-zA-Z0-9_()\s]+)|(?:[а-яА-ЯуёЁ0-9_()\s]+)$/',$surname)&&!empty($surname) ? true:
-			false;
-		}, $surname);
 
-		$first_name_val = call_user_func(function($first_name){
-			return preg_match('/^(?:[a-zA-Z0-9_()\s]+)|(?:[а-яА-ЯуёЁ0-9_()\s]+)$/',$first_name)&&!empty($first_name) ?
-				true: false;
-		}, $first_name);
+			return array(
+				'surname'=>array(
+					'val'=>$surname_val,
+					'value'=>$surname),
+				'first_name'=>array(
+					'val'=>$first_name_val,
+					'value'=>$first_name
+				),
+				'patronymic'=>array(
+					'val'=>$patronymic_val,
+					'value'=>$patronymic),
+				'birth'=>array(
+					'val'=>$birth_val,
+					'day_birth'=>$day_birth,
+					'month_birth'=>$month_birth,
+					'year_birth'=>$year_birth
+				),
+				'city'=>array(
+					'val'=>$city_val,
+					'value'=>$city
+				),
+				'sex'=>array('value'=>$sex),
+				'move'=>array('value'=>$move),
+				'trip'=>array('value'=>$trip),
+				'work_permit'=>array('value'=>$work_permit),
+				'nationality'=>array('value'=>$nationality),
+				'travel_time_work'=>array('value'=>$travel_time_work)
+			);
 
-		$patronymic_val = call_user_func(function($patronymic){
-			return preg_match('/^(?:[a-zA-Z0-9_()\s]+)|(?:[а-яА-ЯуёЁ0-9_()\s]+)$/',$patronymic)&&!empty($patronymic)
-				? true: false;
-		}, $patronymic);
+		}
 
-		$birth_val = call_user_func(function($date){
-			return (is_numeric($date['day_birth'])&&
-				is_numeric($date['month_birth'])&&
-				is_numeric($date['year_birth'])) ||
-			(empty($date['day_birth'])&& empty($date['month_birth'])&& empty($date['year_birth']))? true: false;
-		}, array('day_birth'=>$day_birth, 'month_birth'=>$month_birth, 'year_birth'=>$year_birth));
-
-		//echo $birth_val;
-		return array(
-			'surname'=>array(
-				'val'=>$surname_val,
-				'value'=>$surname),
-			'first_name'=>array(
-				'val'=>$first_name_val,
-				'value'=>$first_name
-			),
-			'patronymic'=>array(
-				'val'=>$patronymic_val,
-				'value'=>$patronymic),
-			'birth'=>array(
-				'val'=>$birth_val,
-				'day_birth'=>$day_birth,
-				'month_birth'=>$month_birth,
-				'year_birth'=>$year_birth
-			)
-		);
-
-	}
-
-	protected function _uploadsAction(){
-		if(isset($_POST['submit_download'])){
-			if(isset($_FILES['photo']) && is_uploaded_file($_FILES['photo']['tmp_name']) && !$_FILES['photo']['error']){
-				$photo = $this->_checkFilesFormatAndSize($_FILES['photo']);
-				if(isset($photo['photo_format']) || isset($photo['photo_size'])){
-					switch($photo){
-						case isset($photo['photo_format']):{
-							return array('helpers'=>array('format_photo'=>'profile/helpers/format_photo'));
-							break;
+		protected function _uploadsAction(){
+			if(isset($_POST['submit_download'])){
+				if(isset($_FILES['photo']) && is_uploaded_file($_FILES['photo']['tmp_name']) && !$_FILES['photo']['error']){
+					$photo = $this->_checkFilesFormatAndSize($_FILES['photo']);
+					if(isset($photo['photo_format']) || isset($photo['photo_size'])){
+						switch($photo){
+							case isset($photo['photo_format']):{
+								return array('helpers'=>array('format_photo'=>'profile/helpers/format_photo'));
+								break;
+							}
+							case isset($photo['photo_size']):{
+								return array('helpers'=>array('photo_size'=>'profile/helpers/photo_size'));
+								break;
+							}
 						}
-						case isset($photo['photo_size']):{
-							return array('helpers'=>array('photo_size'=>'profile/helpers/photo_size'));
-							break;
-						}
+					}else{
+						$this->_movePhotoDir($_FILES['photo']);
+						$this->headerLocation('profile/photo');
 					}
 				}else{
-					$this->_movePhotoDir($_FILES['photo']);
-					$this->headerLocation('profile/photo');
+					return array('helpers'=>array('empty_photo'=>'profile/helpers/empty_photo'));
 				}
 			}else{
-				return array('helpers'=>array('empty_photo'=>'profile/helpers/empty_photo'));
-			}
-		}else{
-			return array();
-		}
-	}
-
-	private function _movePhotoDir($photo){
-		if(isset($photo['name'])){
-			$file_name = $this->getSessionUserID('user').'.'.substr(strrchr($photo['name'], '.'), 1);
-			if(move_uploaded_file ($photo['tmp_name'], $this->_getDownloadDirPhoto() . "/{$file_name}")){
-				$this->_dbuser->updatePhotoId($file_name, $this->getSessionUserID('user'));
+				return array();
 			}
 		}
-	}
 
-	private function _checkFilesFormatAndSize($photo){
-		if(!isset($this->_format_photo[mb_strtolower(substr(strrchr($photo['name'], '.'), 1), "utf-8")])){
-			return array('photo_format'=> true);
+		private function _movePhotoDir($photo){
+			if(isset($photo['name'])){
+				$file_name = $this->getSessionUserID('user').'.'.substr(strrchr($photo['name'], '.'), 1);
+				if(move_uploaded_file ($photo['tmp_name'], $this->_getDownloadDirPhoto() . "/{$file_name}")){
+					$this->_dbuser->updatePhotoId($file_name, $this->getSessionUserID('user'));
+				}
+			}
 		}
-		if($photo['size'] > $this->_size_photo){
-			return array('photo_size'=> true);
+
+		private function _checkFilesFormatAndSize($photo){
+			if(!isset($this->_format_photo[mb_strtolower(substr(strrchr($photo['name'], '.'), 1), "utf-8")])){
+				return array('photo_format'=> true);
+			}
+			if($photo['size'] > $this->_size_photo){
+				return array('photo_size'=> true);
+			}
+			return true;
 		}
-		return true;
+
+		private function _jsPersonal(){
+			return array(
+				'src'=>array(
+					BASE_URL."/public/js/jquery-2.1.1.min.js",
+					BASE_URL."/public/js/jquery.validate.min.js",
+					BASE_URL."/public/js/personal.js"
+				),
+			);
+
+		}
+
+
+
 	}
-
-	private function _jsPersonal(){
-		return array(
-			'src'=>array(
-				BASE_URL."/public/js/jquery-2.1.1.min.js",
-				BASE_URL."/public/js/jquery.validate.min.js",
-				BASE_URL."/public/js/personal.js"
-			),
-		);
-
-	}
-
-
-
-}
 
 
--- mvc/Models/User.php
+++ mvc/Models/User.php
@@ -28,6 +28,99 @@
 			}
 		}
 
+		public function updatePersonal($inputs, $id_user){
+			try {
+				$stmt = $this->_dbc->prepare ("UPDATE
+													profile
+												SET
+													surname = :surname,
+													first_name = :first_name,
+													patronymic = :patronymic,
+													birth = :birth,
+													sex = :sex,
+													city = :city,
+													move = :move,
+													trip = :trip,
+													nationality = :nationality,
+													work_permit = :work_permit,
+													travel_time_work = :travel_time_work
+												WHERE
+													id = :id_user");
+				$stmt->execute(array(
+					':surname'=>$inputs['surname']['value'],
+					':first_name'=>$inputs['first_name']['value'],
+					':patronymic'=>$inputs['patronymic']['value'],
+					':birth'=>"{$inputs['birth']['day_birth']}-{$inputs['birth']['month_birth']}-{$inputs['birth']['year_birth']}",
+					':sex'=>$inputs['sex']['value'],
+					':city'=>$inputs['city']['value'],
+					':move'=>$inputs['move']['value'],
+					':trip'=>$inputs['trip']['value'],
+					':nationality'=>$inputs['nationality']['value'],
+					':work_permit'=>$inputs['work_permit']['value'],
+					':travel_time_work'=>$inputs['travel_time_work']['value'],
+					':id_user'=>$id_user)
+				);
+			}catch (PDOException $e){
+				exit(print_r($e->errorInfo).$e->getFile());
+			}
+		}
+
+		public function selectPersonal($id_user){
+			try {
+				$stmt = $this->_dbc->prepare ("SELECT
+													surname,
+													first_name,
+													patronymic,
+													birth,
+													sex,
+													city,
+													move,
+													trip,
+													nationality,
+													work_permit,
+													travel_time_work
+												FROM
+													profile
+												WHERE
+													id = :id_user");
+				$stmt->execute(array(':id_user'=>$id_user));
+				$personal_data = $stmt->fetch(PDO::FETCH_ASSOC);
+			}catch (PDOException $e){
+				exit(print_r($e->errorInfo).$e->getFile());
+			}
+			$birth  = explode('-',$personal_data['birth']);
+			return array(
+				'surname'=>array(
+					'val'=>true,
+					'value'=>$personal_data['surname']),
+				'first_name'=>array(
+					'val'=>true,
+					'value'=>$personal_data['first_name']
+				),
+				'patronymic'=>array(
+					'val'=>true,
+					'value'=>$personal_data['patronymic']),
+				'birth'=>array(
+					'val'=>true,
+					'day_birth'=>$birth[0],
+					'month_birth'=>$birth[1],
+					'year_birth'=>$birth[2]
+				),
+				'city'=>array(
+					'val'=>true,
+					'value'=>$personal_data['city']
+				),
+				'sex'=>array('value'=>$personal_data['sex']),
+				'move'=>array('value'=>$personal_data['move']),
+				'trip'=>array('value'=>$personal_data['trip']),
+				'work_permit'=>array('value'=>$personal_data['work_permit']),
+				'nationality'=>array('value'=>$personal_data['nationality']),
+				'nationality_other'=>array('value'=>$personal_data['nationality_other']),
+				'work_permit_other'=>array('value'=>$personal_data['work_permit_other']),
+				'travel_time_work'=>array('value'=>$personal_data['travel_time_work'])
+			);
+		}
+
 
 		public function selectPhotoID($id_user){
 			try {
