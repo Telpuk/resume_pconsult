7fed9dfceedaf9e142c8a64f639b897f21e2b7e6
telpuk <telpuk.sergey@gmail.com>
2014-10-17 19:00:57 +0300

    add language

--- public/js/contacts.js
+++ public/js/contacts.js
@@ -1,3 +1,29 @@
-/**
- * Created by telpuk on 10/8/14.
- */
+(function($, window){
+    function Personal(){
+    }
+
+    Personal.prototype.validateForm = function(){
+        $("#personal_contacts").validate({
+            rules:{
+                email:{
+                    required: true,
+                    email: true
+                }
+            },
+            messages:{
+                email:{
+                    required: "Это поле обязательно для заполнения",
+                    email: 'Не коректный email'
+                }
+            }
+        });
+    };
+    Personal.prototype.init = function(){
+        this.validateForm();
+    };
+
+    var personal = new Personal();
+    personal.init();
+
+})(jQuery, window)
+
